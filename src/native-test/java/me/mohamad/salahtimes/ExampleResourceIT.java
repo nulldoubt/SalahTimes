269e04d7237cedf01be937653ec9dd0d40fd7a7f
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2025-04-13 12:23:18 +0200

    Initial Commit. Bismillah.

---
+++ src/native-test/java/me/mohamad/salahtimes/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package me.mohamad.salahtimes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+	// Execute the same tests but in packaged mode.
+}
